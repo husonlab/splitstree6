d749d6efb80d38beacc58e58c6826ed83fcba974
banucetinkaya <bbctnky@gmail.com>
2024-02-06 16:07:45 +0100

    alts nonbinary updated

---
+++ src/main/java/splitstree6/xtra/alts/HybridizationResult.java
@@ -0,0 +1,21 @@
+package splitstree6.xtra.alts;
+
+import java.util.Map;
+
+public class HybridizationResult {
+    private final int hybridizationScore;
+    private final Map<String, String> alignments;
+
+    public HybridizationResult(int hybridizationScore, Map<String, String> alignments) {
+        this.hybridizationScore = hybridizationScore;
+        this.alignments = alignments;
+    }
+
+    public int getHybridizationScore() {
+        return hybridizationScore;
+    }
+
+    public Map<String, String> getAlignments() {
+        return alignments;
+    }
+}
