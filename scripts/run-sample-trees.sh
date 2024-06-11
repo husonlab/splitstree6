1a4a96803c45a1f64789bc6821893fd6b9ff32b5
Daniel <daniel.huson@uni-tuebingen.de>
2024-06-11 15:40:08 +0200

    script for running sample trees

--- scripts/run-sample-trees.sh
+++ scripts/run-sample-trees.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
-root=/Users/huson/IdeaProjects/community/splitstree6/target
+
+bin_dir=$(dirname "$0")         # may be relative path
+bin_dir=$(cd "$bin_dir" && pwd)
+
+root=$bin_dir/../target
 
 jars=$(ls $root/dependency/* | tr '\n' ':')
 
 jars=$jars:$root/SplitsTree-1.0.0-SNAPSHOT.jar
 
-echo java -cp $jars splitstree6.tools.SampleTrees $*
+#echo java -cp $jars splitstree6.tools.SampleTrees $*
 java -cp $jars splitstree6.tools.SampleTrees $*
